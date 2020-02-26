9a5f74e3fcd423433a5c9256ea3d9b9215eb6aa2
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2020-02-26 18:09:40 +0100

    Update Person.js

--- src/main/javascript/Person.js
+++ src/main/javascript/Person.js
@@ -10,6 +10,7 @@ Person.prototype = {
 
     whoAreYou : function() {
         if (1==1) {}
+        if (1==1) {}
         return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
     }
 
